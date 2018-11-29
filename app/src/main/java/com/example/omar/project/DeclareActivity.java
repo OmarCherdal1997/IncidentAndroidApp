8f386d511d9412e7dc33873b575a1aa4231e195d
Omar <omarcherdal.1997@gmail.com>
2018-11-30 00:09:00 +0100

    second commit-navBar

--- app/src/main/java/com/example/omar/project/DeclareActivity.java
+++ app/src/main/java/com/example/omar/project/DeclareActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,11 +21,18 @@ import java.util.List;
 public class DeclareActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG ="DeclareActivity" ;
     ImageView map_btn;
+    Button submit;
     static ImageView  camera_btn;
-    Spinner input_detail, input_province, input_type;
+    Spinner input_detail, input_type;
+    String type, detail,Description,contexte;
+    EditText input_secteur,input_description;
+    Bitmap bitmap;
+    int detailListchoisi=0;
 protected static TextView   langitude,latitude;
-String[] provinces=new String[]{"Agadir Ida-Outanane"," Al Haouz"," Al Hoceïma "," Aousserd"," Assa-Zag"," Azilal","Béni-Mellal"," Benslimane",
-" Berrechid","Berkane","Boujdour "," Boulemane "," Casablanca","Chefchaouen","Chichaoua","Chtouka-Aït Baha ","Driouch ","El Hajeb "," El Jadida","Errachidia "," Essaouira"," Es-Semara","Fès","Kénitra","Rabat ","Tétouan","Tanger-Asilah"};
+String[] typee=new String[]{"Accidents  liés aux usagers","Accidents liés aux intervenants","Accidents liés à l\\'environnement physique extérieur"};
+String[] typee1=new String[]{"vitesse excessive","usagers en forte affluence","panne ou arrêt d\\'un véhicule","usagers circulant à contre sens","Autres.."};
+String[] typee2=new String[]{"défaut d\\'organisation de l\\'exploitation","absence de formation du personnel","absence de mise en place de mesures particulières alors que les conditions minimales d\\'exploitation ne sont plus respectées","Autres.."};
+String[] typee3=new String[]{"avalanche aux têtes","chute de pierres ou effondrements aux têtes","séisme","Autres.."};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,14 +42,73 @@ String[] provinces=new String[]{"Agadir Ida-Outanane"," Al Haouz"," Al Hoceïma 
         langitude=(TextView)findViewById(R.id.langitude);
         latitude=(TextView)findViewById(R.id.latitude);
         input_detail=(Spinner) findViewById(R.id.input_detail);
-        input_province=(Spinner) findViewById(R.id.input_province);
+        input_secteur=(EditText)findViewById(R.id.input_secteur) ;
+        input_description=(EditText)findViewById(R.id.input_description) ;
+        submit=(Button)findViewById(R.id.submit_btn);
+        submit.setOnClickListener(this);
         input_type=(Spinner) findViewById(R.id.input_type);
         map_btn.setOnClickListener(this);
         camera_btn.setOnClickListener(this);
-        ArrayAdapter<CharSequence> provinceAdapter=ArrayAdapter.createFromResource(this,
-                R.array.province, android.R.layout.simple_spinner_item);
-         provinceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-         input_province.setAdapter(provinceAdapter);
+
+        //Spinner adapters
+
+         ArrayAdapter<CharSequence> typeAdapter=ArrayAdapter.createFromResource(this,
+                R.array.type, android.R.layout.simple_spinner_item);
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        input_type.setAdapter(typeAdapter);
+        input_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                type=typee[position];
+                Log.d(TAG,"le type est : "+type);
+                detailListchoisi=position;
+                switch (position){
+                    case 0:
+                        ArrayAdapter<CharSequence> type1Adapter=ArrayAdapter.createFromResource(DeclareActivity.this,
+                                R.array.type1, android.R.layout.simple_spinner_item);
+                        type1Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        input_detail.setAdapter(type1Adapter);
+                    case 1:
+                        ArrayAdapter<CharSequence> type2Adapter=ArrayAdapter.createFromResource(DeclareActivity.this,
+                                R.array.type2, android.R.layout.simple_spinner_item);
+                        type2Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        input_detail.setAdapter(type2Adapter);
+                    case 2:
+                        ArrayAdapter<CharSequence> type3Adapter=ArrayAdapter.createFromResource(DeclareActivity.this,
+                                R.array.type3, android.R.layout.simple_spinner_item);
+                        type3Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        input_detail.setAdapter(type3Adapter);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        input_detail.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(detailListchoisi==1){
+                    detail=typee1[position];
+                    Log.d(TAG,"le detail est : "+detail);
+                }
+                else if(detailListchoisi==2){
+                    detail=typee2[position];
+                    Log.d(TAG,"le detail est : "+detail);
+                }
+                else if(detailListchoisi==3){
+                    detail=typee3[position];
+                    Log.d(TAG,"le detail est : "+detail);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
 
@@ -63,13 +131,21 @@ String[] provinces=new String[]{"Agadir Ida-Outanane"," Al Haouz"," Al Hoceïma 
             case R.id.camera_btn:
                 getImage();
                 break;
+            case R.id.submit_btn:
+                sendData();
+                break;
         }
+    }
+    //Data sending Function
+    private void sendData() {
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap=(Bitmap)data.getExtras().get("data");
+         bitmap=(Bitmap)data.getExtras().get("data");
         camera_btn.setImageBitmap(bitmap);
     }
 }
+
--- app/src/main/java/com/example/omar/project/HomeActivity.java
+++ app/src/main/java/com/example/omar/project/HomeActivity.java
@@ -1,20 +1,69 @@
 package com.example.omar.project;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 Button declare;
+BottomNavigationView nav_bar;
+FrameLayout main_frame;
+HomeFragment homeFragment;
+
+
+FilterFragment filterFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         declare =(Button)findViewById(R.id.declare_btn);
         declare.setOnClickListener(this);
-    }
+        homeFragment=new HomeFragment();
+        filterFragment=new FilterFragment();
+
+
+        main_frame=(FrameLayout) findViewById(R.id.main_frame);
+        nav_bar=(BottomNavigationView)findViewById(R.id.nav_bar);
+        nav_bar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        //nav_bar.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragement(homeFragment);
+                        return true;
+
+                        case R.id.nav_filter:
+                        //nav_bar.setItemTextAppearanceActive();
+                            startActivity(new Intent(HomeActivity.this, FilterActivity.class));
+                        return true;
+
+                        case R.id.nav_map:
+                        //nav_bar.setItemBackgroundResource(R.color.colorPrimary);
+                            startActivity(new Intent(HomeActivity.this, MapsViewerActivity.class));
+                        return true;
+                }
+                return false;
+            }
+
+
+        });}
+
+        private void setFragement(Fragment fragment) {
+            FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.main_frame,fragment,"");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+
 
     @Override
     public void onClick(View v) {
